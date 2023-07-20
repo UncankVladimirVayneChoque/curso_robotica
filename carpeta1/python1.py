6c7377e4a729900498db4ad88e3cd73e9fa08241
UncankVladimirVayneChoque <uncankvaynevladimirchoque@gmail.com>
2023-07-20 19:49:26 -0400

    actualizando bashrc.txt

--- carpeta1/python1.py
+++ carpeta1/python1.py
@@ -1,15 +1,17 @@
+# Alias para el comando 'ls'
 alias mialias1='ls'
 
+# Función 'supergit' para simplificar comandos de Git
 supergit() {
-    if[[ $1 == "update" ]]; then
-        if[[ -z $2 ]]; then
-            echo "Por favor, proporcionr un mensaje de comit."
+    if [[ $1 == "update" ]]; then
+        if [[ -z $2 ]]; then
+            echo "Por favor, proporciona un mensaje de commit."
         else
             echo "---------running git status"
             git status
             echo "---------running git add"
-            git add
-            echo "---------running git commit -m $2"
+            git add .
+            echo "---------running git commit -m \"$2\""
             git commit -m "$2"
             echo "---------running git push"
             git push
@@ -17,6 +19,6 @@ supergit() {
     elif [[ $1 == "server2" ]]; then
         echo "nothing here :)"
     else
-        echo "update: status, add ., commit, push"
+        echo "Uso: supergit update 'mensaje del commit'"
     fi
 }
