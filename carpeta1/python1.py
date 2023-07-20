c92d63aed6c15eebe6954c399d35c52170399a68
UncankVladimirVayneChoque <uncankvaynevladimirchoque@gmail.com>
2023-07-20 19:48:12 -0400

    creando bashrc.txt

--- carpeta1/python1.py
+++ carpeta1/python1.py
@@ -1,1 +1,22 @@
-print("Hola mundo :)")
+alias mialias1='ls'
+
+supergit() {
+    if[[ $1 == "update" ]]; then
+        if[[ -z $2 ]]; then
+            echo "Por favor, proporcionr un mensaje de comit."
+        else
+            echo "---------running git status"
+            git status
+            echo "---------running git add"
+            git add
+            echo "---------running git commit -m $2"
+            git commit -m "$2"
+            echo "---------running git push"
+            git push
+        fi
+    elif [[ $1 == "server2" ]]; then
+        echo "nothing here :)"
+    else
+        echo "update: status, add ., commit, push"
+    fi
+}
